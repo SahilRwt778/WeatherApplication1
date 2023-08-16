7f77759543eba090352e59d29a6a3efb44a30b37
Sahil Rawat <sahilrawatjj@gmail.com>
2023-08-16 12:15:21 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Temprature from './components/Temprature'
+import Temprature from '../components/Temprature'
 function App() {
   
   return (
